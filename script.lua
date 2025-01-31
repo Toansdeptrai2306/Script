93dc109e391f29fa3baad3f3905c072bf74ff964
Toansdeptrai2306 <nguyenminhtoan2306t@gmail.com>
2025-01-31 20:17:46 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -4,15 +4,15 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
 
        local Window = MakeWindow({
          Hub = {
-         Title = "Nguyễn Minh Toàn",
+         Title = "Teddy",
          Animation = "Script tổng hợp"
          },
         Key = {
-        KeySystem = true,
+        KeySystem = false,
         Title = "Key System",
-        Description = "Bạn Cần Get Ket Để Sử Dụng",
-        KeyLink = "https://link4m.com/NNTWW0yi",
-        Keys = {"Thanhh Tuấn"},
+        Description = "",
+        KeyLink = "",
+        Keys = {"Teddy"},
         Notifi = {
         Notifications = true,
         CorrectKey = "Running the Script...",
@@ -23,24 +23,24 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
   })
 
        MinimizeButton({
-       Image = "http://www.roblox.com/asset/?id=137503984014252",
+       Image = "http://www.roblox.com/asset/?id=123809008408464",
        Size = {40, 40},
        Color = Color3.fromRGB(10, 10, 10),
        Corner = true,
        Stroke = false,
        StrokeColor = Color3.fromRGB(255, 0, 0)
       })
-
+      
 ------ Tab
      local Tab1o = MakeTab({Name = "Script Farm"})
-
-
+     
+    
 ------- BUTTON
-
+    
     AddButton(Tab1o, {
      Name = "W azure",
     Callback = function()
-    getgenv().Team = "Pirates"
+	getgenv().Team = "Pirates"
 getgenv().FixCrash = false -- Turn it On For Hopping Server, Improve Performance But Silent Aim On Mob And Player
 getgenv().FixCrash2 = false -- Turn it On For Hopping Server, Improve Performance But Will Remove Speed Changer
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
@@ -48,14 +48,12 @@ loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc61
      })   AddButton(Tab1o, {
      Name = "Redz Hub",
     Callback = function()
-      local Settings = {
-  JoinTeam = "Pirates"; -- Pirates/Marines
-  Translator = true; -- true/false
 loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))(Settings)
-                     }
-              }) AddButton(Tab1o, {
-              Name = "Cụt Tay Hub"
-              Callback - function()
+}
+
+       }) AddButton(Tab1o, {
+        Name = "CutTay Hub" 
+       Callback = function()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/diemquy/CutTayHub/refs/heads/main/Cuttayhubreal.lua'))()
+              }
   end
-  })
